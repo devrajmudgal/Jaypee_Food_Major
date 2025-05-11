928ee994a943cc9ddc0cb1fcd9e27ebbb68c59b6
Devraj Mudgal <devrajmudgal17@gmail.com>
2025-05-11 21:29:42 +0530

    code import

---
+++ backend-spring boot/src/test/java/com/zosh/ZoshFoodApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zosh;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ZoshFoodApplication.class)
+class ZoshFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
